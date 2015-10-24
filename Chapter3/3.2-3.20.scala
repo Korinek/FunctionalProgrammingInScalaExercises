c685b2f094212a3943275d96bc89045c33e0bf36
= <=>
2015-10-24 18:46:57 -0500

    Completed exercise 3.10.

--- Chapter3/3.2-3.20.scala
+++ Chapter3/3.2-3.20.scala
@@ -48,6 +48,11 @@ object List {
         case Cons(x, xs) => f(x, foldRight(xs, z)(f))
     }
 
+    def foldLeft[A,B](as: List[A], z: B)(f: (B, A) => B) : B = as match {
+        case Nil => z
+        case Cons(x, xs) => foldLeft(xs, f(z, x))(f)
+    }
+
     def sum2(ns: List[Int]) =
         foldRight(ns, 0)((x,y) => x + y)
 
