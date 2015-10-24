b3f17e8d1f726ac86957502bb8972c973a2f26b4
Haddon Korinek <haddonkorinek@haddons-mbp.attlocal.net>
2015-10-24 14:34:10 -0500

    Completed exercises 3.1 and 3.2.

---
+++ Chapter3/3.1.scala
@@ -0,0 +1,8 @@
+val x = List(1,2,3,4,5) match {
+    case Cons(x, Cons(2, Cons(4, _))) => x
+    case Nil => 42
+    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+    case _ => 101
+}
+
+This should output the third case, resulting in 1 + 2 = 3.
