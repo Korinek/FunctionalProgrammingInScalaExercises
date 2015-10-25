176f49d6a25cafa886a90f031a736b2904166144
= <=>
2015-10-24 19:54:55 -0500

    Completed exercise 3.22.

--- Chapter3/3.2-3.20.scala
+++ Chapter3/3.2-3.20.scala
@@ -94,6 +94,12 @@ object List {
         concat(map(l)(f))
     }
 
+    def addPairWise[A](a: List[A], b: List[A]) : List[A] = (a, b) match {
+        case (_, Nil) => Nil
+        case (Nil, _) => Nil
+        case (Cons(h1, t1), Cons(h2, t2)) => Cons(h1+h2, addPairWise(t1, t2))
+    }
+
     def sum2(ns: List[Int]) =
         foldRight(ns, 0)((x,y) => x + y)
 
