228bfe58800a1e4fd8a7045c7f219f83ad74d68c
= <haddonkorinek@gmail.com>
2015-10-29 10:18:01 -0500

    Completed exercise 5.2.

--- Chapter5/Streams.scala
+++ Chapter5/Streams.scala
@@ -17,6 +17,18 @@ trait Stream[+A] {
 
         go(this, List()).reverse
     }
+
+    def take(n: Int): Stream[A] = this match {
+        case Cons(h, t) if n > 1  => cons(h(), t().take(n-1)) 
+        case Cons(h, _) if n == 1 => cons(h(), empty)
+        case _ => empty
+    }
+
+    @annotation.tailrec
+    final def drop(n: Int): Stream[A] = this match {
+        case Cons(_, t) if n > 0 => t().drop(n-1)
+        case _ => this
+    }
 }
 
 case object Empty extends Stream[Nothing]
