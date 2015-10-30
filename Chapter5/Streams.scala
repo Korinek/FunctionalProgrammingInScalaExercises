b910bf72016c79bbfddec813ab04cac5061e5bfb
= <haddonkorinek@gmail.com>
2015-10-29 19:31:10 -0500

    Complete exercise 5.10.

--- Chapter5/Streams.scala
+++ Chapter5/Streams.scala
@@ -98,6 +98,13 @@ object Stream {
     }
 
     def from(n: Int): Stream[Int] = {
-        Cons(() => n, from(n+1))
+        cons(n, from(n+1))
+    }
+
+    def fibs: Stream[Int] = {
+        def go(f0: Int, f1: Int): Stream[Int] = {
+            cons(f0, go(f1, f0+f1)) 
+        }
+        go(0, 1) 
     }
 }
