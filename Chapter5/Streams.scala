c426651e64c7c2190aa3aab24d0f9dc99899454d
= <haddonkorinek@gmail.com>
2015-10-29 19:38:35 -0500

    Complete exercise 5.11.

--- Chapter5/Streams.scala
+++ Chapter5/Streams.scala
@@ -107,4 +107,9 @@ object Stream {
         }
         go(0, 1) 
     }
+
+    def unfold[A,S](z: S)(f: S => Option[(A,S)]): Stream[A] = f(z) match {
+        case Some((h,s)) => cons(h, unfold(s)(f))
+        case None => empty
+    }
 }
