8535c1e5d94bee01b374440130ac111d0040c4f5
= <haddonkorinek@gmail.com>
2015-11-01 14:38:07 -0600

    Completed exercise 6.1.

---
+++ Chapter6/State.scala
@@ -0,0 +1,35 @@
+package fpinscala.state
+
+trait RNG {
+    def nextInt: (Int, RNG)
+}
+
+object RNG {
+    
+    case class Simple(seed: Long) extends RNG {
+        def nextInt: (Int, RNG) = {
+            val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+            val nextRNG = Simple(newSeed)
+            val n = (newSeed >>> 16).toInt
+            (n, nextRNG)
+        }
+    }
+
+    type Rand[+A] = RNG => (A, RNG)
+
+    val int: Rand[Int] = _.nextInt
+
+    def unit[A](a: A): Rand[A] =
+        rng => (a, rng)
+
+    def map[A,B](s: Rand[A])(f: A => B): Rand[B] =
+        rng => {
+            val (a, rng2) = s(rng)
+            (f(a), rng2)
+        }
+
+    def nonNegativeInt(rng: RNG): (Int, RNG) = {
+        val (i, r) = rng.nextInt
+        (if (i<0) -(i+1) else i, r)
+    }
+}
