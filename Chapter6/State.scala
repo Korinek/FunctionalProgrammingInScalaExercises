3272a6764b63d264ceeb2df290c692ce501ed722
= <haddonkorinek@gmail.com>
2015-11-01 15:17:21 -0600

    Completed exercise 6.6.

--- Chapter6/State.scala
+++ Chapter6/State.scala
@@ -80,4 +80,12 @@ object RNG {
 
         go(count, rng, List())
     }
+
+    def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A,B) => C): Rand[C] = {
+        rng => {
+            val (a, r1) = ra(rng)
+            val (b, r2) = rb(r1)
+            (f(a,b), r2)
+        }
+    }
 }
