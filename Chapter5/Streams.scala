6222a0cce31b9d5bea0bb0649e3ea64942fda72a
= <haddonkorinek@gmail.com>
2015-11-01 13:04:58 -0600

    Completed exercise 5.16.

--- Chapter5/Streams.scala
+++ Chapter5/Streams.scala
@@ -126,6 +126,18 @@ trait Stream[+A] {
             case s => Some((s, s drop 1))
         } append Stream(empty)
     }
+
+    def hasSubsequence[A](s: Stream[A]): Boolean = {
+        tails exists (_ startsWith s)
+    }
+
+    def scanRight[B](z: B)(f: (A, => B) => B): Stream[B] = {
+        foldRight((z, Stream(z)))((a, p0) => {
+            lazy val p1 = p0
+            val b2 = f(a, p1._1)
+            (b2, cons(b2, p1._2)) 
+        })._2
+    }
 }
 
 case object Empty extends Stream[Nothing]
