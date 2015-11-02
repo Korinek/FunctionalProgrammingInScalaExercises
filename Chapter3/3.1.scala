2242c783861bb3b0d8820856721302f6816e84f4
= <haddonkorinek@gmail.com>
2015-11-02 16:40:35 -0600

    Decided to start over in the book and take things more slowly. Replaced the old code with the exercises included in the fpinscala repo.

--- Chapter3/3.1.scala
+++
@@ -1,8 +0,0 @@
-val x = List(1,2,3,4,5) match {
-    case Cons(x, Cons(2, Cons(4, _))) => x
-    case Nil => 42
-    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
-    case _ => 101
-}
-
-This should output the third case, resulting in 1 + 2 = 3.
