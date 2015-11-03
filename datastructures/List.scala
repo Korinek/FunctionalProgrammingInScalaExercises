9998fbb8a51f6acce25d082802a1eebdb2b16c3f
= <haddonkorinek@gmail.com>
2015-11-03 17:43:26 -0600

    Completed exercise 3.3

--- datastructures/List.scala
+++ datastructures/List.scala
@@ -53,7 +53,10 @@ object List { // `List` companion object. Contains functions for creating and wo
     case Cons(_, t) => t
   }
 
-  def setHead[A](l: List[A], h: A): List[A] = sys.error("todo")
+  def setHead[A](l: List[A], h: A): List[A] = l match {
+    case Nil => sys.error("setHead of empty list")
+    case Cons(_, t) => Cons(h, t)
+  }
 
   def drop[A](l: List[A], n: Int): List[A] = sys.error("todo")
 
