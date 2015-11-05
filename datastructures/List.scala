b76f60f35053d4b7b61a4fadd520d94201975d53
= <haddonkorinek@gmail.com>
2015-11-05 17:36:34 -0600

    Completed exercise 3.23

--- datastructures/List.scala
+++ datastructures/List.scala
@@ -132,4 +132,11 @@ object List { // `List` companion object. Contains functions for creating and wo
       case (_, Nil) => Nil
       case (Cons(h1,t1), Cons(h2,t2)) => Cons(h1+h2, addPairs(t1, t2))
     }
+
+  def zipWith[A](as: List[A], bs: List[A])(f: (A, A) => A): List[A] =
+    (as, bs) match {
+      case (Nil, _) => Nil
+      case (_, Nil) => Nil
+      case (Cons(h1,t1), Cons(h2,t2)) => Cons(f(h1,h2), zipWith(t1,t2)(f))
+    }
 }
