92aceff035aa9938597e943b5d7ce7ba29769842
= <haddonkorinek@gmail.com>
2015-11-06 15:12:36 -0600

    Completed exercise 3.29

--- datastructures/Tree.scala
+++ datastructures/Tree.scala
@@ -4,9 +4,7 @@ sealed trait Tree[+A]
 case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
-
-object Tree {
-
+object Tree { 
   def size[A](t: Tree[A]): Int = t match {
     case Leaf(_) => 1
     case Branch(l, r) => 1 + size(l) + size(r)
@@ -26,4 +24,23 @@ object Tree {
     case Leaf(a) => Leaf(f(a))
     case Branch(l, r) => Branch(map(l)(f), map(r)(f))
   }
+
+  def fold[A,B](t: Tree[A])(f: A => B)(g: (B,B) => B): B = t match {
+    case Leaf(a) => f(a)
+    case Branch(l, r) =>  g(fold(l)(f)(g), fold(r)(f)(g))
+  }
+
+  def size2[A](t: Tree[A]): Int =
+    fold(t)(a => 1)(1 + _ + _)
+
+  def maximum2(t: Tree[Int]): Int =
+    fold(t)(a => a)(_ max _)
+
+  def depth2[A](t: Tree[A]): Int =
+    fold(t)(a => 0)((d1,d2) => 1 + (d1 max d2))
+
+  def map2[A,B](t: Tree[A])(f: A => B): Tree[B] =
+    fold(t)(a => Leaf(f(a)):Tree[B])(Branch(_,_))
+
+
 }
