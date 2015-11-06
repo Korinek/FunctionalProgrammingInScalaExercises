62e809931df8d04415e7f5d0f077bef5a4dd2a6e
= <haddonkorinek@gmail.com>
2015-11-06 14:49:03 -0600

    Completed exercise 3.28

--- datastructures/Tree.scala
+++ datastructures/Tree.scala
@@ -21,4 +21,9 @@ object Tree {
     case Leaf(_) => 0
     case Branch(l, r) => 1 + (size(l) max size(r))
   }
+
+  def map[A,B](t: Tree[A])(f: A => B): Tree[B] = t match {
+    case Leaf(a) => Leaf(f(a))
+    case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+  }
 }
