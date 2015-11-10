dc346492458dd71e0998216a93bb2d64d1798296
= <haddonkorinek@gmail.com>
2015-11-09 18:52:42 -0600

    Completed exercise 5.2

--- laziness/Stream.scala
+++ laziness/Stream.scala
@@ -28,9 +28,16 @@ trait Stream[+A] {
     go(this, List()).reverse
   }
 
-  def take(n: Int): Stream[A] = sys.error("todo")
+  def take(n: Int): Stream[A] = this match {
+    case _ if n <= 0 => empty
+    case Cons(h, t) => cons(h(), t().take(n-1))
+    case _ => empty
+  }
 
-  def drop(n: Int): Stream[A] = sys.error("todo")
+  def drop(n: Int): Stream[A] = this match {
+     case Cons(_, t) if n > 0 => t().drop(n-1)
+     case _ => this
+  }
 
   def takeWhile(p: A => Boolean): Stream[A] = sys.error("todo")
 
