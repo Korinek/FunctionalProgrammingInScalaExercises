404e7f9e7b75532bfcc22f17b77e295066121c3c
= <haddonkorinek@gmail.com>
2015-11-12 20:39:37 -0600

    Completed exercise 6.2

--- state/State.scala
+++ state/State.scala
@@ -32,10 +32,13 @@ object RNG {
 
   def nonNegativeInt(rng: RNG): (Int, RNG) = {
     val (i,r) = rng.nextInt
-    (if (i < 0) -(i+1) else i,, r)
+    (if (i < 0) -(i+1) else i, r)
   }
 
-  def double(rng: RNG): (Double, RNG) = ???
+  def double(rng: RNG): (Double, RNG) = {
+    val (i, r) = nonNegativeInt(rng)
+    (i / (Int.MaxValue.toDouble + 1), r)
+  }
 
   def intDouble(rng: RNG): ((Int,Double), RNG) = ???
 
