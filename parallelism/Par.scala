1242792a8c92eda3c63976615defa2a033d33532
= <haddonkorinek@gmail.com>
2015-11-17 19:59:50 -0600

    Completed exercise 7.11

--- parallelism/Par.scala
+++ parallelism/Par.scala
@@ -81,6 +81,15 @@ object Par {
       if (run(es)(cond).get) t(es) // Notice we are blocking on the result of `cond`.
       else f(es)
 
+  def choiceN[A](n: Par[Int])(choices: List[Par[A]]): Par[A] = 
+    es => {
+      val index = run(es)(n).get
+      run(es)(choices(index))
+    }
+
+  def _choice[A](cond: Par[Boolean])(t: Par[A], f: Par[A]): Par[A] =
+    choiceN(map(cond)(res => if (res) 0 else 1))(List(t, f))
+
   /* Gives us infix syntax for `Par`. */
   implicit def toParOps[A](p: Par[A]): ParOps[A] = new ParOps(p)
 
