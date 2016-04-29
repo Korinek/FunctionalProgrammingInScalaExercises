035db73fec8fb96ba62e34c69f4e4b11e51cccf3
Haddon Korinek <haddonkorinek@gmail.com>
2016-04-28 20:06:56 -0400

    Completed exercise 3.5

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -58,13 +58,18 @@ object List { // `List` companion object. Contains functions for creating and wo
   def setHead[A](l: List[A], h: A): List[A] = 
     Cons(h, tail(l))
 
-  def drop[A](l: List[A], n: Int): List[A] = (l,n) match {
-    case (Nil, _) => Nil
-    case (Cons(_, t), 0) => t
-    case (Cons(h, t), x) => drop(t, x-1)
+  def drop[A](l: List[A], n: Int): List[A] = {
+    if (n <= 0) l
+    else l match {
+      case Nil => Nil
+      case Cons(_, t) => drop(t, n-1)
+    }
   }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = sys.error("todo")
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+    case Cons(h, t) if f(h) =>  dropWhile(t, f)
+    case _ => l
+  }
 
   def init[A](l: List[A]): List[A] = sys.error("todo")
 
