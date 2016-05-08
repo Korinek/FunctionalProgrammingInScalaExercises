8f6a1d9cf4225d6ec1a318dced4905795dbc1cf0
Haddon Korinek <haddonkorinek@gmail.com>
2016-05-08 17:33:16 -0400

    Completed exercise 4.5

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -61,8 +61,15 @@ object Option {
 
   def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
     case Nil => Some(Nil)
-    case h :: t => h flatMap (hh => sequence(t) map (hh :: _))
+    case h::t => h flatMap (hh => sequence(t) map (hh :: _))
   }
 
-  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = sys.error("todo")
+  def sequenceViaTraverse[A](a: List[Option[A]]): Option[List[A]] = {
+    traverse(a)(x => x)
+  }
+
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = a match {
+    case Nil => Some(Nil)
+    case h::t => f(h) flatMap (hh => traverse(t)(f) map (hh :: _))
+  }
 }
