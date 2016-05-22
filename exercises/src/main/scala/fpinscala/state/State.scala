99ed94c158ac386dddc5214fb690dea9c2eb0a8d
Haddon Korinek <haddonkorinek@gmail.com>
2016-05-22 13:38:29 -0400

    Completed exercise 6.7

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -71,6 +71,9 @@ object RNG {
     }
   }
 
+  def intsViaSequence(count: Int)(rng: RNG): Rand[List[Int]] =
+    sequence(List.fill(count)(int))
+
   def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = 
     rng => {
       val (a, r1) = ra(rng)
@@ -78,9 +81,20 @@ object RNG {
       (f(a,b), r2)
     }
 
-  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
+  def sequence[A](xs: List[Rand[A]]): Rand[List[A]] = 
+    xs.foldRight(unit(List[A]()))((x,acc) => map2(x, acc)(_ :: _))
 
-  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
+  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] =
+    rng => {
+      val (a, r) = f(rng)
+      g(a)(r)
+    }
+
+  def nonNegativeLessThan(n: Int): Rand[Int] = 
+    flatMap(nonNegativeInt) { i => 
+      val mod = i % n
+      if (i + (n-1) - mod >= 0) unit(mod) else nonNegativeLessThan(n)
+    }
 }
 
 case class State[S,+A](run: S => (A, S)) {
